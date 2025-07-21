a9733805476b968547bed62596f7433aacebcc04
schooli987 <curriculumfiles@schooli.online>
2025-07-21 13:53:48 +0530

    Add files via upload

---
+++ SA3_template.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+groceries = [
+    {"name": "Corn", "expiry": "2025-02-01"},
+    {"name": "Beans", "expiry": "2024-12-15"},
+    {"name": "Peas", "expiry": "2024-10-05"},
+    {"name": "Tomato Paste", "expiry": "2025-01-10"}
+]
+
