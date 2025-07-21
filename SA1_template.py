8d3fafa83f616708d2dd34720344d1e7535f6aff
schooli987 <curriculumfiles@schooli.online>
2025-07-21 18:15:12 +0530

    Update SA1_template.py

--- SA1_template.py
+++ SA1_template.py
@@ -5,16 +5,3 @@ groceries = [
     {"name": "Butter", "freshness": 75},
     {"name": "Cream", "freshness": 10}
 ]
-
-def bubble_sort_by_freshness(items):
-    n = len(items)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if items[j]['freshness'] > items[j+1]['freshness']:
-                items[j], items[j+1] = items[j+1], items[j]
-
-bubble_sort_by_freshness(groceries)
-
-print("Sorted by Freshness (Low to High):")
-for item in groceries:
-    print(item)
